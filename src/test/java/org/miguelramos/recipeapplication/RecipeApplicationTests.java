315fed2dd513b4fb860289c2491e0c3dbc42a468
elmiguel0 <elmiguelo@live.com.mx>
2020-07-16 23:07:47 -0500

    re-start

---
+++ src/test/java/org/miguelramos/recipeapplication/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miguelramos.recipeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
