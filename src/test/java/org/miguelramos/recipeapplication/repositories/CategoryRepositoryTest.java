1028e01c8018810cbfb08deb37cff753897ac178
elmiguel0 <elmiguelo@live.com.mx>
2020-07-17 21:50:55 -0500

    add test for CategoryRepository and UnitOfMeasureRepository

--- src/test/java/org/miguelramos/recipeapplication/repositories/CategoryRepositoryTest.java
+++ src/test/java/org/miguelramos/recipeapplication/repositories/CategoryRepositoryTest.java
@@ -7,8 +7,7 @@ import org.junit.runner.RunWith;
 import org.miguelramos.recipeapplication.domain.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
@@ -18,9 +17,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 @DataJdbcTest
 class CategoryRepositoryTest {
-    /**
-     * First configure h2db. Create the file name application.yaml in test -> resources path:
-     */
+
     @Autowired
     CategoryRepository categoryRepository;
 
--- src/test/java/org/miguelramos/recipeapplication/repositories/UnitOfMeasureRepositoryTest.java
+++ src/test/java/org/miguelramos/recipeapplication/repositories/UnitOfMeasureRepositoryTest.java
@@ -1,33 +1,37 @@
 package org.miguelramos.recipeapplication.repositories;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.miguelramos.recipeapplication.domain.UnitOfMeasure;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
-@DataJdbcTest
-class UnitOfMeasureRepositoryTest {
+@DataJpaTest
+public class UnitOfMeasureRepositoryTest {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
-    @BeforeEach
-    void setUp() throws Exception{
+    @Before
+    public void setUp() throws Exception {
     }
 
     @Test
-    void findByDescription() throws Exception{
+    public void findByDescription() throws Exception {
+
         Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-        assertEquals("Teaspoon",uomOptional.get().getDescription());
+
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
     }
+
     @Test
     public void findByDescriptionCup() throws Exception {
 
@@ -35,4 +39,5 @@ class UnitOfMeasureRepositoryTest {
 
         assertEquals("Cup", uomOptional.get().getDescription());
     }
+
 }
