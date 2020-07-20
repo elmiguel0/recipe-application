434d412e791d62baa53e6bb0eec8c189e890fdfc
elmiguel0 <elmiguelo@live.com.mx>
2020-07-20 15:47:17 -0500

    adding ImageServiceImplTest

---
+++ src/test/java/org/miguelramos/recipeapplication/service/ImageServiceImplTest.java
@@ -0,0 +1,43 @@
+package org.miguelramos.recipeapplication.service;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.miguelramos.recipeapplication.domain.Recipe;
+import org.miguelramos.recipeapplication.repositories.RecipeRepository;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class ImageServiceImplTest {
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    ImageService imageService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        imageService = new ImageServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void saveImageFile() throws Exception {
+        //given
+        Long id = 1L;
+        MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain",
+                "Spring Framework Guru".getBytes());
+
+    }
+
+}
